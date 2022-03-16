8da9aa9d716e8302e533a95beb666a99bbb14d63
Yan <yandamasceno01@gmail.com>
2022-03-16 10:07:29 -0300

    implementacao nos metodos de ping e listagem de servidores

--- src/main/java/br/com/getarrays/servidor/service/ServidorService.java
+++ src/main/java/br/com/getarrays/servidor/service/ServidorService.java
@@ -2,11 +2,12 @@ package br.com.getarrays.servidor.service;
 
 import br.com.getarrays.servidor.model.Servidor;
 
+import java.io.IOException;
 import java.util.Collection;
 
 public interface ServidorService {
     Servidor criarServidor(Servidor servidor);
-    Servidor ping(String enderecoDeIp);
+    Servidor ping(String enderecoDeIp) throws IOException;
     Collection<Servidor> listarServidores(int qtdDeServidores);
     Servidor pegarServidor(Long id);
     Servidor atualizarServidor(Servidor servidor);
--- src/main/java/br/com/getarrays/servidor/service/implementacao/ServidorServiceImpl.java
+++ src/main/java/br/com/getarrays/servidor/service/implementacao/ServidorServiceImpl.java
@@ -1,14 +1,21 @@
 package br.com.getarrays.servidor.service.implementacao;
 
+import br.com.getarrays.servidor.enumeracao.Status;
 import br.com.getarrays.servidor.model.Servidor;
 import br.com.getarrays.servidor.repositorio.ServidorRepo;
 import br.com.getarrays.servidor.service.ServidorService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.io.IOException;
+import java.net.InetAddress;
 import java.util.Collection;
+
+import static br.com.getarrays.servidor.enumeracao.Status.SERVIDOR_OFF;
+import static br.com.getarrays.servidor.enumeracao.Status.SERVIDOR_ON;
 
 @RequiredArgsConstructor
 @Service
@@ -26,13 +33,19 @@ public class ServidorServiceImpl implements ServidorService {
     }
 
     @Override
-    public Servidor ping(String enderecoDeIp) {
-        return null;
+    public Servidor ping(String enderecoDeIp) throws IOException {
+        log.info("Pinging no servidor IP: {}", enderecoDeIp);
+        Servidor servidor = servidorRepo.procurarPorEnderecoDeIp(enderecoDeIp);
+        InetAddress endereco = InetAddress.getByName(enderecoDeIp);
+        servidor.setStatus(endereco.isReachable(10000) ? SERVIDOR_ON : SERVIDOR_OFF);
+        servidorRepo.save(servidor);
+        return servidor;
     }
 
     @Override
     public Collection<Servidor> listarServidores(int qtdDeServidores) {
-        return null;
+        log.info("Buscando servidores");
+        return servidorRepo.findAll(PageRequest.of(0,qtdDeServidores)).toList();
     }
 
     @Override
